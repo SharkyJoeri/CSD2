d4494a60981911a7099cd65cc602d09b6ba9f0ab
sydeline <straver.joeri@gmail.com>
2023-09-12 12:08:00 +0200

    Hello world script

---
+++ python_basics/playsound.py
@@ -0,0 +1,18 @@
+import simpleaudio
+
+filename = "klaxon.wav"
+sound_object = simpleaudio.WaveObject.from_wave_file(filename)
+
+def soundplay():
+    print("Playing sound", i + 1)
+    play_object = sound_object.play()
+    play_object.wait_done()
+
+print("How many times do you want the sound to play?")
+n = int(input())
+
+for i in range(n): 
+    soundplay()
+
+print("Script done")
+    
